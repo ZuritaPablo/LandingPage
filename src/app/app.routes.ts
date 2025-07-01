6552cc288585deaaa1d4f8f03e1ac228e1b3e075
Pablo Zurita <pablozurita.jr@outlook.com>
2025-06-30 21:22:39 -0500

    Actualizacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { SobreMi } from './sobre-mi/sobre-mi';
+import { Proyectos} from './proyectos/proyectos';
+import { Contactos } from './contactos/contactos';
+import { Home } from './home/home';
+export const routes: Routes = [
+    { path: '', component: Home },
+  { path: 'sobre-mi', component: SobreMi },
+  { path: 'proyectos', component: Proyectos },
+  { path: 'contacto', component: Contactos }
+  
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from './nav/nav';
 import { Footer } from './footer/footer';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav, Footer],
+  imports: [RouterOutlet, Nav, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
